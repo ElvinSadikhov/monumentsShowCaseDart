0d357909ad3a234dbe599d9593c85efeb5e253ca
ElvinSadikhov <elvin.279.6s@gmail.com>
2022-05-07 12:59:03 +0400

    modified files

--- lib/models/responses/quiz_response.dart
+++ lib/models/responses/quiz_response.dart
@@ -17,41 +17,13 @@ class QuizResponse {
 
   final List<Question> questions;
 
-  // QuizResponse({
-  //   required this.uid,
-  //   required this.title,
-  //   required this.correctAnswer,
-  //   required this.answers,
-  // });
-
-  // final int uid;
-  // final String title;
-  // final Answer correctAnswer;
-  // final List<Answer> answers;
-
-  // factory QuizResponse.fromJson(Map<String, dynamic> json) {
   factory QuizResponse.fromJson(List<dynamic> json) {
     return QuizResponse(
         questions: List<Question>.from(json.map((x) => Question.fromJson(x))));
-
-    // return QuizResponse(
-    //   uid: json["uid"],
-    //   title: json["title"],
-    //   correctAnswer: Answer.fromJson(json["correctAnswer"]),
-    //   answers:
-    //       List<Answer>.from(json["answers"].map((x) => Answer.fromJson(x))),
-    // );
   }
 
   List<dynamic> toJson() =>
       List<dynamic>.from(questions.map((x) => x.toJson()));
-
-  // Map<String, dynamic> toJson() => {
-  //       "uid": uid,
-  //       "title": title,
-  //       "correctAnswer": correctAnswer.toJson(),
-  //       "answers": List<dynamic>.from(answers.map((x) => x.toJson())),
-  //     };
 }
 
 class Question {
